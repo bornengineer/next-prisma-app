110c686674b98ea4d734a4e758c2c723fec408f2
Amaan <amkhan@deqode.com>
2023-09-10 03:03:45 +0530

    feat: add todo update and persistance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,35 @@
+import TodoItem from "@/components/TodoItem";
+import prisma from "@/db";
 import Link from "next/link";
 
-export default function Home() {
+function getTodos() {
+  return prisma.todo.findMany();
+}
+
+async function toggleTodo(id: string, complete: boolean) {
+  "use server";
+  await prisma.todo.update({ where: { id }, data: { complete } });
+  console.log("id,complete :>> ", id, complete);
+}
+
+export default async function Home() {
+  const todos = await getTodos();
   return (
-    <header className="flex justify-between items-center mb-4">
-      <h1 className="text-2xl">Todos</h1>
-      <Link
-        className="border border-slate-300 text-slate-300 px-2 py-1 hover:bg-slate-700 focus-within:bg-slate-700 outline-none rounded"
-        href="/new"
-      >
-        New
-      </Link>
-    </header>
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 hover:bg-slate-700 focus-within:bg-slate-700 outline-none rounded"
+          href="/new"
+        >
+          New
+        </Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
+        ))}
+      </ul>
+    </>
   );
 }
